2dbcc072afb245e406520ea55f4d2bc972003bef
Omar AL Zabir <omaralzabir@gmail.com>
2023-04-24 16:33:29 +0100

    Quran mushaf rendering

--- openai_test.py
+++ openai_test.py
@@ -5,13 +5,8 @@ import openai_config
 
 openai.api_key = openai_config.openai_api_key
 #prompt = input('> ');
-prompt = """generate a yaml formatted content.
-For each surah starting from surah fajr in Juz 30 of the Quran as they are ordered
- - on first level, output name of the surah
- - on second level, output surah number, a summary of the surah, and generate a collection of theme names based on granular thematic breakdown of the surah.
- - on third level, output the theme name, generate a summary, verse range, list of tags based on topics found in the verses belonging to the theme
- Ensure verse ranges are contigious and no verse is missed.
- """
+with open("prompt.txt", "r") as f:
+  prompt = f.read();
 
 
 # record the time before the request is sent
@@ -22,7 +17,8 @@ response = openai.ChatCompletion.create(
   messages=[
     {"role": "user", "content": prompt}
   ],
-  temperature=1,
+  temperature=0.2, # more than this results in incorrect verse selection
+  #top_p=0.2,  # try higher number to see if it selects verses incorrectly
   stream=True
 )
 
@@ -46,4 +42,4 @@ with open("openai_output.txt","w") as f:
                 f.flush()
 
 # print the time delay and text received
-print(f"Full response received {chunk_time:.2f} seconds after request")
+print(f"\n\n==========\nFull response received {chunk_time:.2f} seconds after request")
